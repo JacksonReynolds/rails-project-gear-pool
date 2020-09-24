db72d23d129a7fa9beeb61e809f8f29f27a5cf47
JacksonReynolds <jwreynod@gmail.com>
2020-09-24 15:22:40 -0400

    building trip creation actions

--- app/controllers/gear_lists_controller.rb
+++ app/controllers/gear_lists_controller.rb
@@ -1,2 +1,16 @@
 class GearListsController < ApplicationController
+
+    def new
+
+    end
+
+    def create
+
+    end
+
+    private
+
+    def gear_list_params
+        params.require(:gear_list).permit(:name)
+    end
 end
--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,12 @@
 class TripsController < ApplicationController
+    def new
+        @trip = Trip.new
+        @gear_list = @trip.gear_list.build
+    end
+
+    def create
+
+    end
+
+
 end
