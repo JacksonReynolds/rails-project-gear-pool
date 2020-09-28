7e342d9db6133881601da34faa728412e506dd06
JacksonReynolds <jwreynod@gmail.com>
2020-09-28 15:44:49 -0400

    built out very skinny user login and signup functions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,18 @@ class SessionsController < ApplicationController
     end
 
     def create
-        
+        @user = User.find_by(user_name: params[:user][:user_name])
+        if @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect_to user_path
+        else
+            render 'new'
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:user_name, :password)
     end
 end
