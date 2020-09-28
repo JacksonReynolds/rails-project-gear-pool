b04d03a64147eee11bafd2eeae8f7106477a6724
JacksonReynolds <jwreynod@gmail.com>
2020-09-28 13:49:16 -0400

    got nested form to work?

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -10,10 +10,23 @@ class TripsController < ApplicationController
 
     def create
         binding.pry
+        @trip = Trip.new(trip_params)
+        if @trip.save
+            redirect_to trip_path(@trip)
+        else
+            render 'new'
+        end
     end
 
     private
     
-
+    def trip_params
+        params.require(:trip).permit(:destination, :pickup, :dropoff, :gear_list_id, gear_list_attributes: [
+            :name,
+            items_attributes: [
+                :name
+    ]
+])
+    end
 
 end
