a15b4c7907dffcb559a73eea1843ef704f32e359
JacksonReynolds <jwreynod@gmail.com>
2020-09-29 11:04:37 -0400

    added helper for trip index time format

--- app/helpers/trips_helper.rb
+++ app/helpers/trips_helper.rb
@@ -1,5 +1,9 @@
 module TripsHelper
     def show_page_time_format(time)
-        time.strftime('%A %B %e @ %T')
+        time.strftime('%A %B %e @ %R')
+    end
+
+    def index_table_time_format(time)
+        time.strftime('%D @ %R')
     end
 end
