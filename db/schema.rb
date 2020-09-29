742539ec78301e57622f00ddfa36610f95832e4c
JacksonReynolds <jwreynod@gmail.com>
2020-09-29 15:04:35 -0400

    added admin attribute to users for later

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_28_152327) do
+ActiveRecord::Schema.define(version: 2020_09_29_183005) do
 
   create_table "gear_lists", force: :cascade do |t|
     t.string "name"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2020_09_28_152327) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
   end
 
 end
