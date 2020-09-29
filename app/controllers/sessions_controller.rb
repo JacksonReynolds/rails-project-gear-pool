d21f9fc065d8221c2ff385923036855be2a42e97
JacksonReynolds <jwreynod@gmail.com>
2020-09-29 11:00:01 -0400

    couldn't get logout form to work, js clashed with css template, bailed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,6 +13,11 @@ class SessionsController < ApplicationController
         end
     end
 
+    def destroy
+        session.delete :user_id
+        redirect_to root_path
+    end
+
     private
 
     def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@ Rails.application.routes.draw do
   root to: 'application#welcome'
   get '/signin', to: 'sessions#new', as: 'signin'
   get '/signup', to: 'users#new', as: 'signup'
-  delete '/logout', to: 'sessions#destroy', as: 'logout' 
+  post '/logout', to: 'sessions#destroy', as: 'logout' 
   resources :items
   resources :trips
   resources :gear_lists
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
