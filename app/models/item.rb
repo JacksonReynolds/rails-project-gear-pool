cb20b2b15faf2032130c7a983c7ced3a8ab265aa
JacksonReynolds <jwreynod@gmail.com>
2020-10-01 09:51:50 -0400

    gl attr writer for item was creating a new gl although the name was blank, raising validation error, overwrite necessary

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,5 +4,11 @@ class Item < ApplicationRecord
     validates :description, presence: true
     validates :condition, presence: true
 
-    accepts_nested_attributes_for :gear_list
+    # accepts_nested_attributes_for :gear_list
+
+    def gear_list_attributes=(gear_list_attributes)
+        unless gear_list_attributes[:name].blank?
+            self.build_gear_list(name: gear_list_attributes[:name])
+        end
+    end
 end
