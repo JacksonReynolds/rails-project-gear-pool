0ac0cbda38e89c483c5b00ea6cd1b6f81cd7bc59
JacksonReynolds <jwreynod@gmail.com>
2020-10-02 14:26:44 -0400

    added comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,31 @@
 Rails.application.routes.draw do
+  # home
   root to: 'application#welcome'
+
+  # Sign up, in and log out routes
   get '/signin', to: 'sessions#new', as: 'signin'
   post '/signin', to: 'sessions#create'
   get '/signup', to: 'users#new', as: 'signup'
   post '/logout', to: 'sessions#destroy', as: 'logout' 
 
-  resources :items, only: [:index, :show, :new, :create]
-  resources :trips, only: [:create, :update]
+  # for google sign in
+  get "/auth/:provider/callback", to: 'sessions#create_with_google'
   
+  # for GearList scope method demonstration
   get '/most_popular', to: 'gear_lists#most_popular', as: 'most_popular'
+
 
   resources :gear_lists, only: [:index] do
     resources :items, only: [:show]
   end
+  resources :items, only: [:index, :show, :new, :create]
+
 
   resources :users do
     resources :trips, only: [:index, :new, :edit, :destroy, :show]
   end
+  resources :trips, only: [:create, :update]
 
+  # for user profile trip filter
   post '/users/:user_id/trips', to: 'trips#index'
-
-  get "/auth/:provider/callback", to: 'sessions#create_with_google'
 end
